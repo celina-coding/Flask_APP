65a83e9fa67d86ef0870232ada792a2f9d55d5d5
celina-coding <celinafeghoul@gmail.com>
2025-04-15 00:05:35 +0200

    configuring SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+Scss(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"<Task {self.id}>"
 
 @app.route("/")
 def index():
@@ -12,4 +26,7 @@ def index():
 
 
 if __name__ in"__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
