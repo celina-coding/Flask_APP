0068eaf332d23c8cc332d0b1daf7264babd6d8d8
celina-coding <celinafeghoul@gmail.com>
2025-04-15 00:40:41 +0200

    Handle deletion functionnality with error handling

--- app.py
+++ app.py
@@ -41,6 +41,19 @@ def index():
     return render_template("index.html", tasks=tasks)
 
 
+ #delet a task
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        print(f"ERROR: {e}")
+        return "ERROR: {e}"
+
+
 if __name__ in"__main__":
     with app.app_context():
         db.create_all()
